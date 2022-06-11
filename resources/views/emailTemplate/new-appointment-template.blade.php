e4041fbc1759275d800a45cc0403c9c857b46f57
farjanaIslamNipa <nipa.farzana87@gmail.com>
2022-06-11 18:02:07 +0600

    working

--- resources/views/emailTemplate/new-appointment-template.blade.php
+++ resources/views/emailTemplate/new-appointment-template.blade.php
@@ -29,11 +29,23 @@
                     <img style="" src="https://geekscrs.com.au/frontend/images/email-template/booking-confirm-icon.png" alt="">
                 </div>
                 <div style="text-align: center;">
-                    <div style="text-align:center; padding:8px 30px; border-bottom: 1px dashed #656566; display:inline-block;">
+                    <div style="text-align:center; padding:8px 30px; border-bottom: 1px dashed #929292; display:inline-block;">
                         <span style="display: block; font-size:22px; color: #414042;">Booking Confrmed</span>
+                    </div>
+                    <div style="text-align: center; padding-top:10px; border-bottom: 1px solid #acacac;">
+                        <div style="text-align:center; display:inline-block;">
+                            <span style="color:#F38020; font-size:20px; padding-bottom:10px; display:inline-block">19 May, 2022 btween 08:00 AM - 09:00 AM</span>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <main>
+                <div style="padding: 30px 30px;">
+                    <h2 style="color: #F38020; margin-bottom:1px;">Hi Dinesh B <span>(VIC)</span></h2>
+                    <p style="font-size: 16px; color:#414042;">We are pleased to let you know, we have booked a computer appointment for you.</p>
+                </div>
+            </main>
         </div>
         <hr style="border: 2px solid #FFB602">
         <div style="margin-top:10px;background-color:#F38020; padding:30px 0;">
